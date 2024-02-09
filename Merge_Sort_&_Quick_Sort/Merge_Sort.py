0e5c2e39ad0faa8ae93192167a0947fefc59d6f6
Om Patel <omhpatel7@gmail.com>
2024-02-09 15:10:01 -0500

    Update Merge_Sort.py

--- Merge_Sort_&_Quick_Sort/Merge_Sort.py
+++ Merge_Sort_&_Quick_Sort/Merge_Sort.py
@@ -1,1 +1,27 @@
+def mergeSort(array):
+    if len(array) > 1:
+        middle = len(array) // 2
+        left = array[:middle]
+        right = array[middle:]
 
+        mergeSort(left)
+        mergeSort(right)
+
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                array[k] = left[i]
+                i += 1
+            else:
+                array[k] = right[j]
+                j += 1
+            k += 1
+        
+        while i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            array[k] = right[j]
+            j += 1
+            k += 1
